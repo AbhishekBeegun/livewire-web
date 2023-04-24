3725f504421cd353fbc630e8bd3863897ae90b16
Dooshyant Beegun <abhishekbeegun0642@gmail.com>
2023-04-24 15:24:16 +0400

    updateinapptsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,15 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 import React from "react";
 import "~/styles/globals.css";
 import Navbar from "~/components/Navbar/Navbar";
 import Footer from "~/components/BasicLayout/Footer";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+const MyApp = ({Component, pageProps} :any) => {
   return (
-    <SessionProvider session={session}>
+    <>
       <Navbar/>
       <Component {...pageProps} />
       <Footer/>
-    </SessionProvider>
+    </>
   );
 };
 
