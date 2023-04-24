567e526ca0fa8382d942eb282573b6ed9125ff03
Dooshyant Beegun <abhishekbeegun0642@gmail.com>
2023-04-24 15:19:26 +0400

    react-eslint-added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import React from 'react'
-
+import React from "react";
 import "~/styles/globals.css";
 import Navbar from "~/components/Navbar/Navbar";
 import Footer from "~/components/BasicLayout/Footer";
