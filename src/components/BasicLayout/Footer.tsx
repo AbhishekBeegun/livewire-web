3579845c2c44ab0ea49837691e7517af9d2f7628
Dooshyant Beegun <abhishekbeegun0642@gmail.com>
2023-05-27 08:28:29 +0400

    Footer-updated

--- src/components/BasicLayout/Footer.tsx
+++ src/components/BasicLayout/Footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {IoLogoInstagram,IoLogoFacebook} from "react-icons/io"
+import {IoLogoAndroid} from 'react-icons/io5'
+import Link from 'next/link'
 
 const Footer = () => {
     const company = [
@@ -47,12 +49,20 @@ const Footer = () => {
             {/* socials */}
             <div className='w-full'>
     
-            <div className="flex flex-row justify-between lg:justify-around gap-2 py-1 text-black">
+            <div className="flex flex-row justify-between items-center lg:justify-around gap-2 py-1 text-black">
             <p className="text-white font-semibold text-sm text-right">SOCIAL</p>
             <span className='flex gap-5'>
                 <IoLogoInstagram color='white'/>         
                 <IoLogoFacebook color='white' />
             </span>
+
+
+            <button className="px-4 py-2 flex justify-center items-center gap-2 bg-white rounded-full font-semibold hover:scale-105 transition-all text-black">
+               <IoLogoAndroid size={20}/> 
+              <Link target='_blank' href={"https://drive.google.com/uc?export=download&id=1zKVjFDRMgqftN3eYA0HEGF12dLEc9Bsn"}
+
+              >Download LiveWire Apk</Link>
+            </button>
             </div>
     
             <p className="font-semibold text-xs py-2 text-center">2023 Abhishek Beegun.ALL rights reversed.</p>
